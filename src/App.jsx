5d280a14b5485d42f93d5b7f2e986481d32e0e9c
Your Name <portia894@gmail.com>
2023-12-14 12:57:14 -0500

    this was interesting

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 import { puppyList } from './data.js'
 
+
 function App() {
-  console.log(puppyList);
+  
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList);
   return (
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-      {/* Your JSX code here */}
+    <div className='App'>
+   { 
+   puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>;
+   })
+}
     </div>
   )
 }
